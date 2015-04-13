5af445d097c8ea1781c7d782d551dee94262e84b
Chevelle <davebz@gmail.com>
2015-04-13 16:19:51 -0400

    config files

---
+++ customize_target_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=`pwd`
+
+cd $DIR
+echo "Copy update-binary"
+cp -r other/update-binary out/target_files/OTA/bin/updater
+
+echo "Copy GuardProvider"
+cd ..
+cp -r miui/XXHDPI/system/app/GuardProvider.apk $DIR/out/target_files/SYSTEM/app/
